929ebcc660601ae19f72bb2d6027e67e4ed7c957
David-Vermaak <kaamrev1@gmail.com>
2024-08-11 16:38:56 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module davidv.music_player {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens davidv.music_player to javafx.fxml;
+    exports davidv.music_player;
+}
